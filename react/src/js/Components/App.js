2de785b565bba1a104c2ef67f5b6283fcb4afce3
joelespinozaro <joelespinozaro@gmail.com>
2020-01-28 15:40:43 -0500

    Uso de react con webpack

--- react/src/js/Components/App.js
+++ react/src/js/Components/App.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data.json";
+import Loader from "./Loader";
 
 function App() {
-  return <div>App</div>;
+  const [loaderList, setLoaderList] = useState([]);
+
+  function handleClick() {
+    setLoaderList(data.loaders);
+  }
+  return (
+    <div>
+      <h1>App list</h1>
+      <ul>
+        {loaderList.map(item => (
+          <Loader data={item} key={item.id} />
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Mostrar item</button>
+    </div>
+  );
 }
 export default App;
---
+++ react/src/js/Components/Loader.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Loader(props) {
+  return <li>{props.data.name}</li>;
+}
+export default Loader;
